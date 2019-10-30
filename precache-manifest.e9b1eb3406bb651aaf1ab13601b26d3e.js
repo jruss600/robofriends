82039640e2518dcba02b0abd1e4dc7bf0775c55d
John Russell <jruss600@gmail.com>
2019-10-29 20:32:34 -0600

    Updates

---
+++ precache-manifest.e9b1eb3406bb651aaf1ab13601b26d3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "5282459d92950e1bae1a",
+    "url": "/robofriends/static/js/main.5282459d.chunk.js"
+  },
+  {
+    "revision": "fc6baf1fc3b3686ab690",
+    "url": "/robofriends/static/js/1.fc6baf1f.chunk.js"
+  },
+  {
+    "revision": "5282459d92950e1bae1a",
+    "url": "/robofriends/static/css/main.fdebd9d8.chunk.css"
+  },
+  {
+    "revision": "fc6baf1fc3b3686ab690",
+    "url": "/robofriends/static/css/1.2b7066ca.chunk.css"
+  },
+  {
+    "revision": "4baa62472d518b0f60ee47f0a69db600",
+    "url": "/robofriends/index.html"
+  }
+];
